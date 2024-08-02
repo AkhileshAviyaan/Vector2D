4d3faaad4302cbdbc653fa6e9d56f3cf89cf314e
akhilesh.aviyaan@gmail.com <akhilesh.aviyaan@gmail.com>
2024-08-02 20:54:06 +0545

    Add project files.

---
+++ Vector2D/Vector2D.vb
@@ -0,0 +1,20 @@
+﻿Public Class Vector2D
+	Public XOrigin As Double
+	Public YOrigin As Double
+	Public DelX As Double
+	Public DelY As Double
+
+	Public Sub New()
+
+	End Sub
+	Public Sub New(originx As Double, originy As Double, delx As Double, dely As Double)
+		Me.XOrigin = originx
+		Me.XOrigin = originx
+		Me.XOrigin = originx
+		Me.XOrigin = originx
+	End Sub
+	Public Sub New(delx As Double, dely As Double)
+		Me.DelX = delx
+		Me.DelY = dely
+	End Sub
+End Class
